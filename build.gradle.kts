af3de7c05784bd4efa573fb23971675894eb8653
shinyay <shinya.com@gmail.com>
2021-06-24 23:45:32 +0900

    Add annotation processor

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jboss.resteasy:resteasy-jackson2-provider:4.6.1.Final")
 	implementation("org.keycloak:keycloak-dependencies-server-all:14.0.0")
