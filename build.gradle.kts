22ff345a98a58c028c84b0cc54a3357e45072152
shinyay <shinya.com@gmail.com>
2021-06-24 21:51:46 +0900

    Modify dependencies for Keycloak

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jboss.resteasy:resteasy-jackson2-provider:4.6.1.Final")
+	implementation("org.keycloak:keycloak-dependencies-server-all:14.0.0")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	runtimeOnly("com.h2database:h2")
