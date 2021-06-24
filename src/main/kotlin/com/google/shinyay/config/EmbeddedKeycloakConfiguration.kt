84d3c3ac306d469fdd5e9faf53951d644379259d
shinyay <shinya.com@gmail.com>
2021-06-24 23:43:45 +0900

    Add JaxRS dispathcer servler registration class

--- src/main/kotlin/com/google/shinyay/config/EmbeddedKeycloakConfiguration.kt
+++ src/main/kotlin/com/google/shinyay/config/EmbeddedKeycloakConfiguration.kt
@@ -1,7 +1,11 @@
 package com.google.shinyay.config
 
+import org.jboss.resteasy.plugins.server.servlet.HttpServlet30Dispatcher
+import org.springframework.boot.web.servlet.ServletRegistrationBean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class EmbeddedKeycloakConfiguration {
+
+    fun keycloakJaxRsRegistration(): ServletRegistrationBean<HttpServlet30Dispatcher> {}
 }
