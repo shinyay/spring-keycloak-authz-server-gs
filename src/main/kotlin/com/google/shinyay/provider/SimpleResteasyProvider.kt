71c7bc935edecbd225ef21d88bee04281317bbc9
shinyay <shinya.com@gmail.com>
2021-06-27 14:44:35 +0900

    Add initilize context data function

--- src/main/kotlin/com/google/shinyay/provider/SimpleResteasyProvider.kt
+++ src/main/kotlin/com/google/shinyay/provider/SimpleResteasyProvider.kt
@@ -1,10 +1,12 @@
 package com.google.shinyay.provider
 
+import org.jboss.resteasy.spi.ResteasyProviderFactory
 import org.keycloak.common.util.ResteasyProvider
 
 class SimpleResteasyProvider : ResteasyProvider {
     override fun <R : Any?> getContextData(type: Class<R>?): R {
-        TODO("Not yet implemented")
+        ResteasyProviderFactory.getInstance()
+        return ResteasyProviderFactory.getContextData(type)
     }
 
     override fun pushDefaultContextObject(type: Class<*>?, instance: Any?) {
